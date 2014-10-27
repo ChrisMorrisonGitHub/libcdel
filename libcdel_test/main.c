25057b1c5e878e084f0e45e9e36f2abc1acaa3eb
Chris Morrison <chris-morrison@cyberservices.com>
2014-10-27 00:17:31 +0000

    Small changes to tests

---
+++ libcdel_test/main.c
@@ -0,0 +1,166 @@
+/*
+ * File:   main.c
+ * Author: Chris Morrison
+ *
+ * Created on 06 October 2014, 14:21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "../src/libcdel.h"
+
+char *read_string(FILE *fp);
+
+/*
+ *
+ */
+int main(int argc, char** argv)
+{
+    int cline = 0;
+    char base2[300];
+    char base2spc[300];
+    char base16upc[128];
+    char base16lwr[128];
+    char base32[128];
+    char base58[128];
+    char base64[128];
+    char base85[128];
+    char base91[128];
+    char *line = NULL;
+
+    FILE *fp = fopen("./EncodingTestFile.txt", "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "ERROR: Failed to open the test file (%s)\n", strerror(errno));
+        return 1;
+    }
+
+    line = read_string(fp);
+    if ((line == NULL) || (line[0] == '\0'))
+    {
+        fprintf(stderr, "ERROR: Failed to read from test file (%s)\n", strerror(errno));
+        return 1;
+    }
+
+    if (strcmp(line, "#Base2<TAB>Base2Spaced<TAB>Base85<TAB>Base16<TAB>Base32<TAB>Base58<TAB>Base64<TAB>Base91") != 0)
+    {
+        fprintf(stderr, "ERROR: The token line in the test file is missing, not valid or this test utility to too old.\n");
+        return 1;
+    }
+    free(line);
+
+    fprintf(stdout, "C Data Encoding Library (libcdel) Test Utility\nStarting tests ...\n\n");
+
+    while ((line = read_string(fp)) != NULL)
+    {
+        if (line[0] == '\0') continue;
+
+        memset(base2, 0, 300);
+        memset(base2spc, 0, 300);
+        memset(base16upc, 0, 128);
+        memset(base16lwr, 0, 128);
+        memset(base32, 0, 128);
+        memset(base58, 0, 128);
+        memset(base64, 0, 128);
+        memset(base85, 0, 128);
+        memset(base91, 0, 128);
+
+        if (sscanf(line, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s", base2, base2spc, base85, base16upc, base32, base58, base64, base91) != 8)
+        {
+            fprintf(stderr, "ERROR: A corrupt, damaged or badly formed line was encountered.\n");
+            free(line);
+            return 1;
+        }
+
+        ++cline;
+
+        /* Test Base2 */
+
+        fprintf(stdout, "Test of base2 verification function, string %02d  (sans spaces)... ", cline);
+        if (cdel_verify_base2_string(base2) == 0)
+        {
+            fprintf(stdout, "\aFAILED\nVerify failed with input %s\n", base2);
+            exit(1);
+        }
+        else
+        {
+            fprintf(stdout, "PASSED\n");
+        }
+        fprintf(stdout, "Test of base2 verification function, string %02d (spaced)...       ", cline);
+        if (cdel_verify_base2_string(base2spc) == 0)
+        {
+            fprintf(stderr, "\aFAILED\nVerify failed with input %s", base2spc);
+            exit(1);
+        }
+        else
+        {
+            fprintf(stdout, "PASSED\n");
+        }
+
+        /* Test Base16 */
+
+        fprintf(stdout, "Test of base16 verification function, string %02d  (uppercase)...  ", cline);
+        if (cdel_verify_base16_string(base16upc) == 0)
+        {
+            fprintf(stdout, "\aFAILED\nVerify failed with input %s\n", base2);
+            exit(1);
+        }
+        else
+        {
+            fprintf(stdout, "PASSED\n");
+        }
+        /*fprintf(stdout, "Test of base2 verification function, string %02d (lowercase)...    ", cline);
+        if (cdel_verify_base16_string(base16lwr) == 0)
+        {
+            fprintf(stderr, "\aFAILED\nVerify failed with input %s", base2spc);
+            exit(1);
+        }
+        else
+        {
+            fprintf(stdout, "PASSED\n");
+        } */
+
+        free(line);
+    }
+
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+
+char *read_string(FILE *fp)
+{
+    size_t slen = 0;
+    char buffer[4096];
+    char *out_buffer = NULL;
+    size_t idx1 = 0;
+    size_t idx2 = 0;
+    char c = 0;
+
+    if (fp == NULL) return NULL;
+    if ((feof(fp) != 0) || (ferror(fp) != 0)) return NULL;
+    memset(buffer, 0, 4096);
+    if (fgets(buffer, 4096, fp) == NULL) return NULL;
+    slen = strlen(buffer);
+    if ((slen == 0) || (buffer[0] == '\n') || (buffer[0] == '\r')) return "";
+    out_buffer = (char *) malloc(slen + 2); /* Put an extra NULL on the end just to make sure. */
+    if (out_buffer == NULL) return NULL;
+    memset(out_buffer, 0, (slen + 2));
+
+    while ((c = buffer[idx1]) != '\0')
+    {
+        if ((c != '\r') && (c != '\n'))
+        {
+            out_buffer[idx2] = c;
+            ++idx2;
+        }
+
+        ++idx1;
+    }
+
+    slen = strlen(out_buffer);
+    if (slen == 0) return "";
+
+    return out_buffer;
+}
